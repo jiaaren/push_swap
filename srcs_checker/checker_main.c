35885dc1c8a1e38191c7a6a2d4b085518e520e2d
jiaren <jiaaren@github.com>
2021-06-22 14:55:25 +0800

    added consideration over input as string

--- srcs_checker/checker_main.c
+++ srcs_checker/checker_main.c
@@ -6,13 +6,37 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 19:56:16 by jkhong            #+#    #+#             */
-/*   Updated: 2021/06/20 19:58:57 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/06/22 14:53:43 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doubly_linked_list.h"
 #include "common_utils.h"
 #include "libchecker.h"
+
+void	free_split(char **tmp_char)
+{
+	char	**ptr;
+
+	ptr = tmp_char;
+	while (*ptr)
+		free(*ptr++);
+	free(tmp_char);
+}
+
+// returns length of array
+int	give_array(char ***argv)
+{
+	char	**tmp_char;
+	int		arrsize;
+
+	tmp_char = ft_split((*argv)[1], ' ');
+	arrsize = 0;
+	while (tmp_char[arrsize])
+		arrsize++;
+	*argv = tmp_char;
+	return (arrsize);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,9 +45,17 @@ int	main(int argc, char *argv[])
 	t_dstack	stacks;
 
 	if (argc == 1)
-		return (1);
-	arrsize = argc - 1;
-	arr = verify_input(arrsize, &argv[1]);
+		return (0);
+	if (argc == 2)
+		arrsize = give_array(&argv);
+	else
+	{
+		argv = &argv[1];
+		arrsize = argc - 1;
+	}
+	arr = verify_input(arrsize, argv);
+	if (argc == 2)
+		free_split(argv);
 	if (arr)
 	{
 		stacks = make_stacks(arr, arrsize);
--- srcs_pushswap/pushswap_main.c
+++ srcs_pushswap/pushswap_main.c
@@ -6,13 +6,14 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 19:35:24 by jkhong            #+#    #+#             */
-/*   Updated: 2021/06/21 20:32:45 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/06/22 14:49:32 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doubly_linked_list.h"
 #include "libpushswap.h"
 #include "common_utils.h"
+#include "libft.h"
 
 static void	sort_stack_simple(int *arr, int arrsize)
 {
@@ -67,6 +68,30 @@ static void	sort_stack_median(int *arr, int arrsize)
 	free_stacks(&stacks);
 }
 
+void	free_split(char **tmp_char)
+{
+	char	**ptr;
+
+	ptr = tmp_char;
+	while (*ptr)
+		free(*ptr++);
+	free(tmp_char);
+}
+
+// returns length of array
+int	give_array(char ***argv)
+{
+	char	**tmp_char;
+	int		arrsize;
+
+	tmp_char = ft_split((*argv)[1], ' ');
+	arrsize = 0;
+	while (tmp_char[arrsize])
+		arrsize++;
+	*argv = tmp_char;
+	return (arrsize);
+}
+
 int	main(int argc, char *argv[])
 {
 	int	arrsize;
@@ -74,8 +99,16 @@ int	main(int argc, char *argv[])
 
 	if (argc == 1)
 		return (0);
-	arrsize = argc - 1;
-	arr = verify_input(arrsize, &argv[1]);
+	if (argc == 2)
+		arrsize = give_array(&argv);
+	else
+	{
+		argv = &argv[1];
+		arrsize = argc - 1;
+	}
+	arr = verify_input(arrsize, argv);
+	if (argc == 2)
+		free_split(argv);
 	if (arr)
 	{
 		if (arrsize <= 10)
