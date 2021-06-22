e092154dd62723e3b38c5555eb1d6ba848a90f0b
jiaren <jiaaren@github.com>
2021-06-22 15:10:33 +0800

    Consolidated into a single makefile

--- sh_del_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make fclean -C submit_pushswap
-make fclean -C submit_checker
-rm push_swap
-rm checker
--- sh_make_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make -C submit_pushswap
-make -C submit_checker
-cp submit_pushswap/push_swap push_swap
-cp submit_checker/checker checker
